d78cb1a1c5743cf4b4c191a886df221d56e6d32b
CiaraBee <65850640+CiaraBee@users.noreply.github.com>
2022-11-09 21:28:07 +0000

    Update app.py imports from folders

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, redirect, request, Blueprint
 import os
 from models.models import models, db
-from models.add_task import add_task
-from models.update_task import update_task
-from models.delete_task import delete_task
+from routes.add_task import add_task
+from routes.update_task import update_task
+from routes.delete_task import delete_task
+from forms.forms import InputTodo
 
 # Get base directory
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -13,6 +14,8 @@ app = Flask(__name__)
 # Set up db config to local path
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'test.db')
+
+app.config['SECRET_KEY'] = 'secret'
 
 # Register db model blueprint
 app.register_blueprint(models)
